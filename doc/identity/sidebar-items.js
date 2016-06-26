5b01ceefb4270a34fccf21e01c579244975f2187
Alexander Ronald Altman <alexanderaltman@me.com>
2016-06-25 21:37:17 -1000

    Docs for v0.0.1

---
+++ doc/identity/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Refl",""]],"trait":[["Equals",""],["Identity","An identity type; that is, the type of “equality witnesses.”"]]});
---
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['identity'] = ["impl&lt;A: ?<a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html' title='core::marker::Sized'>Sized</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='identity/struct.Refl.html' title='identity::Refl'>Refl</a>&lt;A&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
