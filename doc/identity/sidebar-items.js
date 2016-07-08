a75677c79be2dabf71077d629b7b7c9535087672
Alexander Ronald Altman <alexanderaltman@me.com>
2016-07-07 17:07:38 -1000

    Docs for v0.0.4

--- doc/identity/sidebar-items.js
+++ doc/identity/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["helpers",""],["lift",""]],"struct":[["Refl",""]],"trait":[["Equals",""],["Identity","An identity type; that is, the type of “equality witnesses.”"],["IdentityUtil",""]]});
+initSidebarItems({"macro":[["ty_list!",""]],"mod":[["lift",""]],"struct":[["Refl",""]],"trait":[["Equals",""],["Identity","An identity type; that is, the type of “equality witnesses.”"]]});
--- doc/implementors/identity/lift/trait.TyFun.js
+++ doc/implementors/identity/lift/trait.TyFun.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['identity'] = [];implementors['identity'] = [];implementors['identity'] = [];implementors['identity'] = [];implementors['identity'] = [];implementors['identity'] = [];implementors['identity'] = [];implementors['identity'] = [];implementors['identity'] = [];implementors['identity'] = [];implementors['identity'] = [];
+implementors["identity"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
