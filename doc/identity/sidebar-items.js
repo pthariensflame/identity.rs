96f9c2e357f42848fd1a06a691e1e8a1f34ec8b5
Alexander Ronald Altman <alexanderaltman@me.com>
2016-07-15 17:03:58 -0700

    Docs for v0.0.6

--- doc/identity/sidebar-items.js
+++ doc/identity/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["refl",""]],"macro":[["ty_list!",""]],"mod":[["lift",""]],"struct":[["Refl",""]],"trait":[["Equals",""],["Identity","An identity type; that is, the type bound of “equality witnesses.”"]]});
+initSidebarItems({"fn":[["refl",""]],"macro":[["ty_list!",""]],"mod":[["elim_helpers",""],["lift",""]],"struct":[["Refl",""]],"trait":[["Equals",""],["Identity","An identity type; that is, the type bound of “equality witnesses.”"]]});
